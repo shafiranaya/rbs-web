599795466b40d7f2f641ab886be97a8c0e42bde3
Shafira Naya <shfrnaya@gmail.com>
2021-11-07 13:02:47 +0700

    delete dump and finish styling

--- dump/main_clipspy.py
+++
@@ -1,9 +0,0 @@
-from clips import Environment
-
-env = Environment()
-
-if __name__ == "__main__":
-    env.clear()
-    env.load("lip-products-recommendation.clp")
-    for rule in env.rules():
-        print(rule)
